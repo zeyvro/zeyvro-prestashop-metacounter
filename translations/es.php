d2dda1d24353e2084bcac5fd64f79f41a1243e31
google644 <google@sensabien.com>
2026-07-05 09:22:56 +0200

    chore: barrido oficial php-dev-tools (autoindex+header-stamp+cs-fixer) + .gitattributes
    - .php-cs-fixer.dist.php: quitar 'translations' del exclude (regresion REGLAS-DURAS §trampas)
    - autoindex: index.php anadido en translations/ (carpeta sin index.php)
    - header-stamp: cabecera @license normalizada (free/MIT)
    - php-cs-fixer: estilo @Symfony aplicado
    - .gitattributes: actualizado a forma canonica (EOL LF + binarios)
    - Sin ocurrencias de escape sin UTF-8 en este modulo
    
    No toca classes/ZeyvroModuleTrait.php mas alla del barrido normal (baseline estaba limpio en este modulo).

--- translations/es.php
+++ translations/es.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Zeyvro PrestaShop Module
+ *
+ * @author    Zeyvro <admin@zeyvro.com>
+ * @copyright 2026 Zeyvro
+ * @license   https://opensource.org/licenses/MIT  MIT License
+ */
 if (!defined('_PS_VERSION_')) {
     exit;
 }
